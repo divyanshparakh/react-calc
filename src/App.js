71a38d7da84245970566e4fee972b341ba7e659e
Divyansh Parakh <divyanshparakh11@gmail.com>
2023-10-23 16:18:15 +0530

    Layout Created | Functionality needs to be added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Just Basic React App
-        </p>
-      </header>
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'>123,234 *</div>
+        <div className='current-operand'></div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>Del</button>
+      <button>+</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
     </div>
   );
 }
